61e14354085df348c4e2109e6d48351e93047551
sudarshan24 <sudarshan@intraedge.com>
2026-04-11 19:15:48 -0400

    containerized app, added homepage

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### website_v1
 This is the first version of my personal website project.
+
+## Server-side
+
+This website is built using Flask. 
+
+## Front-end
+
+So far we have just html.
+
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, render_template
 
 web_app_bp = Blueprint("webapp_routes", __name__)
 
 @web_app_bp.route("/", methods=["GET"])
-def root():
-    return jsonify("API is now working")
+def homepage():
+    return render_template('index.html')
 
 def setup(app:Flask):
     app.register_blueprint(web_app_bp, url_prefix="")
