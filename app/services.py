a89b31e119f25978a817522ca5ab77da02e35fc2
sudarshan24 <sudarshan@intraedge.com>
2026-04-12 13:21:57 -0400

    added new service

--- app/services.py
+++ app/services.py
@@ -7,11 +7,27 @@ def set_post(title: str, content: str):
         cursor = conn.cursor()
         cursor.execute(
             """
-            UPDATE blog_posts 
-            SET created_date = CURRENT_TIMESTAMP, post_title = ?, post_content = ?;
+                UPDATE blog_posts 
+                SET created_date = CURRENT_TIMESTAMP, post_title = ?, post_content = ?;
             """, (title, content)
         )
         conn.commit()
+        conn.close()
+
+    except sqlite3.OperationalError as e:
+        return e
+    
+def get_last_post():
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+                SELECT post_title, post_content FROM blog_posts ORDER BY post_id DESC LIMIT 1;"
+            """
+        )
+        last_post = cursor.fetchone()
+        return last_post[0] if last_post else "No data found"
 
     except sqlite3.OperationalError as e:
         return e
