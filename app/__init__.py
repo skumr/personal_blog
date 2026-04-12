214e235c2161f49ad22bcdc0292a7dd139aa0236
sudarshan24 <sudarshan@intraedge.com>
2026-04-12 16:17:01 -0400

    added services, displaying post content

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from init_db import create_table
 from routes import setup
 
 web_app = Flask("web_app", template_folder="app/templates")
 
+create_table()
 setup(web_app)
 
 if __name__ == "__main__":
