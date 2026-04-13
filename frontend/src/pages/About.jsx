5ba0de399593e97a5cc7163aa21dc4214a6706de
sudarshan24 <sudarshan@intraedge.com>
2026-04-13 17:29:47 -0400

    built out v2 of website

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,17 @@
+export function About() {
+  return (
+   <article
+      style={{
+        border: '1px solid #ddd',
+        padding: '1rem',
+        marginBottom: '1rem',
+      }}
+    >
+      <h2>About Me</h2>
+      <p>
+        Hey everyone, welcome to my blog! I started this blog site as a personal project to engage in frontend/backend web development.
+        Follow my posts here on my journey documenting my development projects and thoughts, as I work through problems and find solutions!
+      </p>
+    </article>
+  );
+}
