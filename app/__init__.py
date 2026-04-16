86275d67386db6d6cf6e7198674b145df96fb700
sudarshan24 <sudarshan@intraedge.com>
2026-04-16 19:32:53 -0400

    containerized blog

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,15 @@ from init_db import create_table
 from routes import setup
 
 web_app = Flask(__name__)
-CORS(web_app)
+CORS(
+    web_app,
+    resources={r"/api/*": {"origins": "*"}},
+    allow_headers=["Content-Type"],
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+)
 
 create_table()
 setup(web_app)
 
 if __name__ == "__main__":
-    web_app.run(debug=True)
+    web_app.run(host="0.0.0.0", port=5000, debug=True)
